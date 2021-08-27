f731efcacca4ff672e47b2dd6068bc9cb29d6196
suriya1410 <79642202+suriya1410@users.noreply.github.com>
2021-08-27 20:20:07 +0530

    Update init.py

--- init.py
+++ init.py
@@ -5,5 +5,5 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World Suriya'
 
-if __name__ == '__main__':
-   app.run(host="0.0.0.0", port=80)
+#if __name__ == '__main__':
+ #  app.run(host="0.0.0.0", port=80)

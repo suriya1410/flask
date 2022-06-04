f2a76eafb6c78dfb5b6f04fa0292d63a013c7ba6
suriya1410 <79642202+suriya1410@users.noreply.github.com>
2022-06-04 19:13:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,14 @@
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
-import mysql.connector
 import logging
-import boto3
-import json
-
-client = boto3.client('secretsmanager')
-response = client.get_secret_value(SecretId='RSDSecret')
 app = Flask(__name__)
 logging.basicConfig(filename='flask.log', level=logging.INFO,format='%(levelname)s:%(message)s')
 
-app.config['MYSQL_HOST'] = secretDict['host'],
-app.config['MYSQL_USER'] = secretDict['username'],
-app.config['MYSQL_PASSWORD'] = secretDict['password'],
-app.config['MYSQL_DB'] = "regform"
+app.config['MYSQL_HOST'] = 'database-my.caomyyms75ok.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_USER'] = 'suriya'
+app.config['MYSQL_PASSWORD'] = 'suriya123'
+app.config['MYSQL_DB'] = 'regform'
 
-mydb = mysql.connector.connect(
-  host=secretDict['host'],
-  user=secretDict['username'],
-  password=secretDict['password'],
-)
-
-mycursor = mydb.cursor()
-
-mycursor.execute("CREATE DATABASE regform")
-
-
-mydb = mysql.connector.connect(
-  host=secretDict['host'],
-  user=secretDict['username'],
-  password=secretDict['password'],
-  database="regform"
-)
-
-mycursor = mydb.cursor()
-
-mycursor.execute("CREATE TABLE userdata(name VARCHAR(150), age INT(3), email VARCHAR(150), mobile VARCHAR(10), location VARCHAR(100));")
 
 
 mysql = MySQL(app)
@@ -52,6 +25,7 @@ def index():
         mobile = details['mobile']
         location = details['location']
         cur = mysql.connection.cursor()
+        cur.execute("CREATE TABLE IF NOT EXISTS userdata(name VARCHAR(150), age INT(3), email VARCHAR(150), mobile VARCHAR(10), location VARCHAR(100))") 
         cur.execute("INSERT INTO userdata(name, age, email, mobile, location) VALUES (%s, %s, %s, %s, %s)", (name, age, email, mobile, location))
         mysql.connection.commit()
         cur.close()
@@ -61,12 +35,11 @@ def index():
 
 @app.route('/users')
 def users():
-    cur =mysql.connection.cursor()
+    cur = mysql.connection.cursor()
     resultValue = cur.execute("SELECT * FROM userdata")
     if resultValue > 0:
         usersDetails = cur.fetchall()
-
         return render_template('users.html',usersDetails=usersDetails)
 
 if __name__ == '__main__':
-  app.run(host="0.0.0.0",port=80)
+    app.run(host="0.0.0.0",port=80,debug=True)
